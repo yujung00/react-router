7c1e0102ecb1e41877b68318e4a2c391288e889f
yujung00 <ung5468@naver.com>
2022-06-30 11:05:40 +0900

    # 공통 레이아웃 컴포넌트

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import {About, Home} from "./pages";
 import Profile from "./pages/Profile";
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
+import Layout from "./Layout";
 
 class App extends Component{
 
     render() {
         return (
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/profiles/:username" element={<Profile/>}/>
+                <Route element={<Layout/>}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/profiles/:username" element={<Profile/>}/>
+                </Route>
                 <Route path="/articles" element={<Articles/>}>
                     <Route path=":id" element={<Article/>}/>
                 </Route>
